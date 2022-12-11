b56f8069fa2195a7cbee7962da132a74e4be42ca
mbrazalez <117437155+mbrazalez@users.noreply.github.com>
2022-12-11 01:28:24 +0100

    2nd Sprint: - Maps: Draw route & get locations - NFC Writer & Reader - QR Scanner
    WORK IN PROCESS %.%.%.%
    - Adaptable for all devices
    - Drag & drop memory game

---
+++ app/src/main/java/com/pk/memapp20/Adapter/ToDoAdapter.java
@@ -0,0 +1,118 @@
+package com.pk.memapp20.Adapter;
+
+import static android.content.Context.VIBRATOR_SERVICE;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.os.Vibrator;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.pk.memapp20.AddNewTask;
+import com.pk.memapp20.R;
+import com.pk.memapp20.todo_apu;
+import com.pk.memapp20.Model.ToDoModel;
+
+import java.util.List;
+
+
+public class ToDoAdapter extends RecyclerView.Adapter<ToDoAdapter.MyViewHolder> {
+
+    private List<ToDoModel> todoList;
+    private todo_apu activity;
+    private FirebaseFirestore firestore;
+    Vibrator vibrator;
+
+    public ToDoAdapter(todo_apu todoApu , List<ToDoModel> todoList, Vibrator dildofelcot){
+        this.todoList = todoList;
+        activity = todoApu;
+        this.vibrator=dildofelcot;
+
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(activity).inflate(R.layout.each_task , parent , false);
+        firestore = FirebaseFirestore.getInstance();
+
+        return new MyViewHolder(view);
+    }
+
+    public void deleteTask(int position){
+        ToDoModel toDoModel = todoList.get(position);
+        firestore.collection("task").document(toDoModel.TaskId).delete();
+        todoList.remove(position);
+        notifyItemRemoved(position);
+    }
+    public Context getContext(){
+        return activity;
+    }
+    public void editTask(int position){
+        ToDoModel toDoModel = todoList.get(position);
+
+        Bundle bundle = new Bundle();
+        bundle.putString("task" , toDoModel.getTask());
+        bundle.putString("due" , toDoModel.getDue());
+        bundle.putString("id" , toDoModel.TaskId);
+
+        AddNewTask addNewTask = new AddNewTask();
+        addNewTask.setArguments(bundle);
+        addNewTask.show(activity.getSupportFragmentManager() , addNewTask.getTag());
+    }
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+
+        ToDoModel toDoModel = todoList.get(position);
+        holder.mCheckBox.setText(toDoModel.getTask());
+
+        holder.mDueDateTv.setText("Due On " + toDoModel.getDue());
+
+        holder.mCheckBox.setChecked(toBoolean(toDoModel.getStatus()));
+
+        holder.mCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked){
+                    vibrator.vibrate(1000);
+                    firestore.collection("task").document(toDoModel.TaskId).update("status" , 1);
+                }else{
+                    vibrator.vibrate(1000);
+                    firestore.collection("task").document(toDoModel.TaskId).update("status" , 0);
+                }
+            }
+        });
+
+    }
+
+    private boolean toBoolean(int status){
+        return status != 0;
+    }
+
+    @Override
+    public int getItemCount() {
+        return todoList.size();
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder{
+
+        TextView mDueDateTv;
+        CheckBox mCheckBox;
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            mDueDateTv = itemView.findViewById(R.id.due_date_tv);
+            mCheckBox = itemView.findViewById(R.id.mcheckbox);
+
+        }
+    }
+}
---
+++ app/src/main/java/com/pk/memapp20/Model/TaskId.java
@@ -0,0 +1,17 @@
+package com.pk.memapp20.Model;
+
+
+import androidx.annotation.NonNull;
+
+import com.google.firebase.firestore.Exclude;
+
+public class TaskId {
+    @Exclude
+    public String TaskId;
+
+    public  <T extends  TaskId> T withId(@NonNull final String id){
+        this.TaskId = id;
+        return  (T) this;
+    }
+
+}
